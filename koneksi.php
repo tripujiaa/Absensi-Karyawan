61522e8ebc1ee034f4bf2a3f56ab91b2e3f85aa2
Retno Ires <fairy.edensor@gmail.com>
2024-12-28 00:11:58 +0700

    fix:koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,6 +2,6 @@
 $koneksi = mysqli_connect("localhost", "root", "", "karyawansi");
 
 if (mysqli_connect_errno()) {
-	echo "koneksi gagal " . mysql_connect_error();
+	echo "koneksi gagal " . mysqli_connect_error();
 }
  ?>
